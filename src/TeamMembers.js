a8f7d22135b64623fa4ed2428ddd4031f2ff1374
Michael Darden <michaeldarden@michaels-mbp.ddns.uark.edu>
2022-11-28 15:22:49 -0600

    First Commit Cont

---
+++ src/TeamMembers.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+let TeamMembers = () => {
+  return (
+    <div>
+      Hey this is the Team member Page!
+    </div>
+  )
+}
+
+export default TeamMembers
---
+++ src/TopBar.js
@@ -0,0 +1,64 @@
+import * as React from 'react'
+import AppBar from '@mui/material/AppBar'
+import Box from '@mui/material/Box'
+import Toolbar from '@mui/material/Toolbar'
+import Typography from '@mui/material/Typography'
+import IconButton from '@mui/material/IconButton'
+import MenuIcon from '@mui/icons-material/Menu'
+import MenuItem from '@mui/material/MenuItem'
+import Menu from '@mui/material/Menu'
+import TeamMembers from './TeamMembers'
+
+let TopBar = () => {
+  const [anchorEl, setAnchorEl] = React.useState(null)
+
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget)
+  }
+
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
+
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            onClick={handleMenu}
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Menu
+            id="menu-appbar"
+            anchorEl={anchorEl}
+            anchorOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+            }}
+            open={Boolean(anchorEl)}
+            onClose={handleClose}
+          >
+            <MenuItem onClick={handleClose}>Team Members</MenuItem>
+            <MenuItem onClick={handleClose}>My account</MenuItem>
+          </Menu>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            News
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
+
+export default TopBar
